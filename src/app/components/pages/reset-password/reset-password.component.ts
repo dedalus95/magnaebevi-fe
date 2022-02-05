5926f0d9d3c4de58530e8d24d25e212bd6a887f5
Emanuele Cerilli <ecerilli1@gmail.com>
2022-02-05 19:26:56 +0100

    finish login, register, reset, change

---
+++ src/app/components/pages/reset-password/reset-password.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UtenteService } from 'src/app/services/utente.service';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['./reset-password.component.scss']
+})
+export class ResetPasswordComponent implements OnInit {
+
+  constructor(private utenteService: UtenteService) { }
+
+  resetFormGroup: FormGroup = new FormGroup({
+    username : new FormControl("", [Validators.required])
+  })
+
+  reset() {
+    let currentForm = this.resetFormGroup;
+    let hasHappened = false;
+    this.utenteService.resetPassword(currentForm.value.username).subscribe(res => {
+      alert('reset avvenuto con successo, controlla la tua mail! 🥳')
+    }, err => {
+      if(err.status == 400) 
+          alert('Il programmatore è una capra, perché ti ha permesso di inserire una stringa vuota 😡');
+      
+      if(err.status == 401)
+          alert('hai inserito un username sbagliato. 😡');
+    });
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  
+}
